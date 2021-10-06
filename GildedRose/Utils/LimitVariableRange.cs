b8f7dd4021a1f8fdcd22e32db0dd59475c8129d0
Nikita Driuma <Nichita.Driuma@endava.com>
2021-10-06 14:52:35 +0300

    added Utils with LimitRange extensions for Quality and SellIn

---
+++ GildedRose/Utils/LimitVariableRange.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GildedRose.Utils
+{
+    public static class LimitVariableRange
+    {
+        public static int LimitQualityRange(this int quality)
+        {
+            if (quality >= 0)
+            {
+                if (quality <= 50)
+                {
+                    return quality;
+                }
+
+                return 50;
+            }
+
+            return 0;
+        }
+
+        public static int LimitSellInRange(this int sellIn)
+        {
+            if (sellIn >= 0)
+            {
+                return sellIn;
+            }
+            return 0;
+        }
+    }
+}
